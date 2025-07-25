c964bd0c7ac90d1423475e65fe305029f989afb1
IureLima <iurelimasilva@gmail.com>
2025-07-25 18:30:02 -0300

    feat: add security configuration

---
+++ src/main/java/org/recipe/recipebookmanager/configs/security/SecurityConfigurations.java
@@ -0,0 +1,41 @@
+package org.recipe.recipebookmanager.configs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(HttpMethod.POST, "/auth/login").permitAll(); //Permite que qualquer pessoa tenha acesso a fazer o login
+                    authorize.requestMatchers(HttpMethod.POST, "/auth/login").permitAll(); //Permite que qualquer um faça requisição de registro
+                    authorize.requestMatchers(HttpMethod.POST, "/recipe").hasRole("ADMIN"); //Para o post na rota fornecida temos a role de admin
+                    authorize.anyRequest().authenticated();//para as outras rotas, temos que nós autenticar
+                }).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
